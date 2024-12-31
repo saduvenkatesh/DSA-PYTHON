b3cae64205ef574b2ace160eee4ef3b84fa0352f
SADU VENKATESH <saduvenkatesh141205@gmail.com>
2024-12-31 09:23:51 +0530

    insertion sorting

---
+++ sorting/insertion.sort.py
@@ -0,0 +1,11 @@
+# Insertion Sort
+# Insertion sort method sorts a list of elements by inserting each successive element in the previously sorted sublist
+# Such insertion of elements requires the other elements to be shuffled as required
+
+class sorting :
+    def __init__(self, arr):
+        self.list=arr
+        self.size=len(arr)
+    
+    def insertion_sort(self):
+        pass
